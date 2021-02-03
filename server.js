b897b7b0cc6f050458b366e574a9839dbd28420f
adilhansatemirovv@gmail.com <adilhansatemirovv@gmail.com>
2021-02-03 22:21:22 +0600

    feat: add cors

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const mailRouter = require('./routes/mail');
 app.use('/mail', mailRouter);
+
+app.get('/', (req, res) => {
+  res.send('Works');
+});
 
 app.listen(3030, () => console.log('server started'));
